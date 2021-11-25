7175eaea624d791a9c4fdaa323b5b9642a43a112
Goldeaur <34507362+Master-Goldeau@users.noreply.github.com>
2021-11-25 14:51:36 +0100

    connection database ok

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS reader (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  first_name varchar(250) DEFAULT NULL,
+  last_name varchar(250) DEFAULT NULL,
+  phone varchar(250) DEFAULT NULL,
+  email varchar(250) DEFAULT NULL,
+  city varchar(250) DEFAULT NULL,
+  creation_date BIGINT not null,
+  last_modification_date BIGINT not null,
+  status varchar(50) DEFAULT NULL,
+);
+
