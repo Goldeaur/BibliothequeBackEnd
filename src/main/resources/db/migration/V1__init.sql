0487230361c4739d696dff3895ee58cb41db74ea
Goldeaur <34507362+Master-Goldeau@users.noreply.github.com>
2021-12-05 19:47:37 +0100

    maj database for security

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,20 +1,21 @@
-CREATE TABLE IF NOT EXISTS reader (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  first_name varchar(250) DEFAULT NULL,
-  last_name varchar(250) DEFAULT NULL,
-  phone varchar(250) DEFAULT NULL,
-  email varchar(250) DEFAULT NULL,
-  city varchar(250) DEFAULT NULL,
-  creation_date BIGINT not null,
-  last_modification_date BIGINT not null,
-  status varchar(50) DEFAULT NULl);
-
-CREATE TABLE IF NOT EXISTS credentials (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
   login varchar(250) DEFAULT NULL,
   password varchar(250) DEFAULT NULL,
   phone varchar(250) DEFAULT NULL,
   email varchar(250) DEFAULT NULL,
-  creation_date BIGINT not null,
-  last_modification_date BIGINT not null,
-  role varchar(50) DEFAULT 'READER' not null);
+  creation_date DATE not null DEFAULT (CURRENT_DATE),
+  last_modification_date DATE not null DEFAULT (CURRENT_DATE),
+  role varchar(50) DEFAULT 'reader' not null);
+
+
+CREATE TABLE IF NOT EXISTS reader (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  first_name varchar(250) DEFAULT NULL,
+  last_name varchar(250) DEFAULT NULL,
+  city varchar(250) DEFAULT NULL,
+  creation_date DATE not null DEFAULT (CURRENT_DATE),
+  last_modification_date DATE not null DEFAULT (CURRENT_DATE),
+  credentials_id bigint not null,
+  status varchar(50) DEFAULT null,
+  CONSTRAINT FK_credentials FOREIGN KEY (credentials_id)
+  REFERENCES credentials(id)  ON DELETE CASCADE);
