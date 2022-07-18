8f95f6d72f9bab7e3515270b1e15cd7d27ba3c90
Goldeaur <34507362+Master-Goldeau@users.noreply.github.com>
2022-07-18 15:11:30 +0200

    CORS CONFIG

--- src/main/java/com/bibliotheque/config/SecurityConfig.java
+++ src/main/java/com/bibliotheque/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.bibliotheque.repository.CustomCredentialsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
@@ -13,6 +14,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebFluxSecurity
 @Configuration
@@ -34,12 +41,12 @@ public class SecurityConfig {
      *
      * @return MapReactiveUserDetailsService an InMemory implementation of user details
      */
-   @Bean
+    @Bean
     public MapReactiveUserDetailsService userDetailsRepository() {
         UserDetails user = User.withUsername("bibliotheque")
                 .password(
                         passwordEncoder().encode(
-                        "Gigi&Titi"
+                                "Gigi&Titi"
                         )
                 )
                 .roles("ADMIN")
@@ -57,11 +64,13 @@ public class SecurityConfig {
                 .switchIfEmpty(Mono.defer(() -> Mono.error(new LoginException("login Not Found"))));
     }*/
 
-    @Bean
+/*    @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
         return http
-                .csrf().disable()
-                .authorizeExchange()
+                .csrf().disable().cors(corsSpec -> corsSpec.configurationSource(
+                        corsMediaSpotConfigurationSource()
+                )).authorizeExchange()
+                .matchers(new PathPatternParserServerWebExchangeMatcher("/**", HttpMethod.OPTIONS)).permitAll()
                 .pathMatchers("/").permitAll()
                 .anyExchange().authenticated()
                 .and()
@@ -69,10 +78,34 @@ public class SecurityConfig {
                 .and()
                 .formLogin().disable()
                 .build();
-    }
+    }*/
 
     @Bean
-    PasswordEncoder passwordEncoder(){
+    UrlBasedCorsConfigurationSource corsMediaSpotConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+/*    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("http://allowed-origin.com"));
+        corsConfig.setMaxAge(8000L);
+        corsConfig.addAllowedMethod("POST");
+        corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedHeader("Baeldung-Allowed");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return new CorsWebFilter(source);
+    }*/
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
